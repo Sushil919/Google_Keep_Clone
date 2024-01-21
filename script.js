3924f1a5581543569eee60ce2e5b7c66c56ca998
Sushil <sushilkumar811544@gmail.com>
2024-01-21 10:06:03 +0530

    update data in local Storage

--- script.js
+++ script.js
@@ -4,27 +4,44 @@ const addNoteButton = document.getElementById('addNote');
 const notesDiv = document.getElementById('notes');
 let notes = [];
 
+
+
 function addNotes(){
+    let notes = localStorage.getItem('notes');
+    if(notes == null){
+        notes = [];
+    }
+    else{
+        notes = JSON.parse(notes);
+    }
     if(addText.value == ''){
         alert('Add your note')
         return;
     }
+
     const noteObj = {
         title: addTitle.value,
         note: addText.value
     }
     notes.push(noteObj);
+    localStorage.setItem('notes', JSON.stringify(notes));
     showNotes();
 }
 
 function showNotes(){
     let notesHTML = '';
+    let notes = localStorage.getItem('notes');
+    if(notes === null){
+        return;
+    }else{
+        notes = JSON.parse(notes);
+    }
     for(let i=0; i<notes.length; i++){
         console.log(notes[i])
         notesHTML += `
                 <div class="note">
-                    <button>Delete</button>
-                    <div class="title">${notes[i].title}</div>
+                    <button class="deleteNote" >Delete</button>
+                    <div class="title">${notes[i].title === "" ? 'Note' : notes[i].title}</div>
                     <div class="text">${notes[i].text}</div>
                 </div>
         `
